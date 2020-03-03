756fe2e7e6958f91d03def0775dd01d9c0b227be
--global <--global>
2020-03-03 20:23:28 +0100

    Making bash install script smoother

--- motivate/install.sh
+++ motivate/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be with sudo" 1>&2
+   echo "This script must be run with sudo" 1>&2
    exit 1
 fi
 
 INSTALLDIR="/opt/motivate"
 
 # Create motivate folder
-mkdir -p $INSTALLDIR
-
+if [[ -d ${INSTALLDIR} ]]; then
+  mkdir -p $INSTALLDIR
+fi
 # Copy the datafolder and set permissions
-cp -r $PWD/data $INSTALLDIR/
+cp  -a $PWD/data $INSTALLDIR/
 chmod -R 777 $INSTALLDIR/data
 
 # Copy and link the executable
